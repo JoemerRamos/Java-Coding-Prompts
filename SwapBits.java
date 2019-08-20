a7a4fc2cc1f16331e5d7902198d778715ead7711
Joemer Ramos <34356209+JoemerRamos@users.noreply.github.com>
2019-08-20 17:32:03 -0400

    Solutions that were worked on from EPI Java
    Some of these are still a WIP since I needed to use the code from their solutions in order for me to understand the concepts

---
+++ SwapBits.java
@@ -0,0 +1,21 @@
+package epi;
+import epi.test_framework.EpiTest;
+import epi.test_framework.GenericTest;
+public class SwapBits {
+  @EpiTest(testDataFile = "swap_bits.tsv")
+  public static long swapBits(long x, int i, int j) {
+    if(((x >>> i) & 1) != (((x >>> j) & 1))){
+      x ^= (1L << i);
+      x ^= (1L << j); // 1 left shifted 14
+    }
+    return x;
+  }
+
+  public static void main(String[] args) {
+    System.exit(
+        GenericTest
+            .runFromAnnotations(args, "SwapBits.java",
+                                new Object() {}.getClass().getEnclosingClass())
+            .ordinal());
+  }
+}
