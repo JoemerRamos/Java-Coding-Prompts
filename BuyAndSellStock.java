fcafea2d5c87a39aea40a26cfb6ea1a773abbbc9
Joemer Ramos <34356209+JoemerRamos@users.noreply.github.com>
2019-08-21 19:05:38 -0400

    O(n) solution
    Buying and selling one stock for maximum profit. Uses a divide and conquer algorithm.

---
+++ BuyAndSellStock.java
@@ -0,0 +1,23 @@
+package epi;
+import epi.test_framework.EpiTest;
+import epi.test_framework.GenericTest;
+import java.util.List;
+public class BuyAndSellStock {
+  @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
+  public static double computeMaxProfit(List<Double> prices) {
+    double maxProfit = 0, minPrice = Integer.MAX_VALUE;
+    for(double price: prices){
+      maxProfit = Math.max(maxProfit, price - minPrice);
+      minPrice = Math.min(price, minPrice);
+    }
+    return maxProfit;
+  }
+
+  public static void main(String[] args) {
+    System.exit(
+        GenericTest
+            .runFromAnnotations(args, "BuyAndSellStock.java",
+                                new Object() {}.getClass().getEnclosingClass())
+            .ordinal());
+  }
+}
