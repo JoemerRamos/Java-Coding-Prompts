5953e3bfb922449790e5d8cd33a6791e09c485d3
Joemer Ramos <34356209+JoemerRamos@users.noreply.github.com>
2019-08-31 16:26:54 -0400

    EPI Valid Sudoku Problem

---
+++ IsValidSudoku.java
@@ -0,0 +1,53 @@
+package epi;
+import epi.test_framework.EpiTest;
+import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+public class IsValidSudoku {
+  @EpiTest(testDataFile = "is_valid_sudoku.tsv")
+
+  // Check if a partially filled matrix has any conflicts.
+  public static boolean isValidSudoku(List<List<Integer>> partialAssignment) {
+    //Check row
+    for (int i = 0; i < partialAssignment.size(); ++i){
+      if(hasDuplicate(partialAssignment, i, i+ 1, 0, partialAssignment.size())) return false;
+    }
+
+    //Check col
+    for (int i = 0; i < partialAssignment.size(); i++){
+      if(hasDuplicate(partialAssignment, 0, partialAssignment.size(), i, i + 1)) return false;
+    }
+
+    int regionSize = (int)Math.sqrt(partialAssignment.size());
+    for (int I = 0; I < regionSize; ++I){
+      for (int J = 0; J < regionSize; ++J){
+        if (hasDuplicate(partialAssignment, regionSize * I, regionSize *(I + 1), regionSize * J, regionSize * (J + 1)))return false;
+      }
+    }
+
+    return true;
+  }
+
+  private static boolean hasDuplicate(List<List<Integer>> partialAssignment, int startRow, int endRow, int startCol, int endCol){
+    List<Boolean> isPresent = new ArrayList <>(Collections.nCopies(partialAssignment.size() + 1, false));
+    for (int i = startRow; i < endRow; ++i){
+      for(int j = startCol; j < endCol; ++j){
+        if (partialAssignment.get(i).get(j) != 0 && isPresent.get(partialAssignment.get(i).get(j))) return true;
+        isPresent.set(partialAssignment.get(i).get(j), true);
+      }
+    }
+    return false;
+  }
+
+
+  public static void main(String[] args) {
+    System.exit(
+        GenericTest
+            .runFromAnnotations(args, "IsValidSudoku.java",
+                                new Object() {}.getClass().getEnclosingClass())
+            .ordinal());
+  }
+}
